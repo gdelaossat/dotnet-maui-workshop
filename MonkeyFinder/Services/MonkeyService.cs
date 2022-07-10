09b86983ae2bc0a084262e285014c1750b212533
Gelder de la Ossa <dimitrigel@hotmail.com>
2022-07-10 17:13:14 -0500

    first commit

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,34 @@
+﻿namespace MonkeyFinder.Services;
+using System.Net.Http.Json;
+
+public class MonkeyService
+{
+
+    List<Monkey> monkeyList = new();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        return monkeyList;
+
+    }
+
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+
+
+
+
+}
